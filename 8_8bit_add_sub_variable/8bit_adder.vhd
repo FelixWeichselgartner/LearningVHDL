f39093ee22e4c8811808e68eb7573593cc68d71c
Felix Weichselgartner <felix.weichselgartner@t-online.de>
2020-10-29 18:58:36 +0100

    formatting

--- 8_8bit_add_sub_variable/8bit_adder.vhd
+++ 8_8bit_add_sub_variable/8bit_adder.vhd
@@ -7,13 +7,13 @@ USE ieee.std_logic_unsigned.ALL; -- add std_logic_vectors
 ENTITY adder IS
     -- all ports of type std_logic / std_logic_vector
     PORT (
-        a : IN std_logic_vector(7 DOWNTO 0);
-        b : IN std_logic_vector(7 DOWNTO 0);
-        m : IN std_logic;       -- mode 1: add; mode 0: subtract
-        Q : OUT std_logic_vector(7 DOWNTO 0);
-        c : OUT std_logic;      -- carry of result
-        s : OUT std_logic;      -- if 1 result is negative
-        zero : OUT std_logic);  -- if 1 result is zero
+        a : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+        b : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+        m : IN STD_LOGIC; -- mode 1: add; mode 0: subtract
+        Q : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+        c : OUT STD_LOGIC; -- carry of result
+        s : OUT STD_LOGIC; -- if 1 result is negative
+        zero : OUT STD_LOGIC); -- if 1 result is zero
 END adder;
 
 ARCHITECTURE arch OF adder IS
@@ -21,35 +21,35 @@ BEGIN
 
     -- combinatorical logic
     PROCESS (a, b, m)
-        variable number1, number2 : signed(7 DOWNTO 0);
-        variable result : signed(8 DOWNTO 0) := "000000000";
+        VARIABLE number1, number2 : signed(7 DOWNTO 0);
+        VARIABLE result : signed(8 DOWNTO 0) := "000000000";
     BEGIN
         -- default values
         Q <= x"00";
         c <= '0';
         s <= '0';
         zero <= '0';
-        
+
         number1 := signed(a);
         number2 := signed(b);
-        
+
         -- subtraction
-        if (m = '0') then
-            number2 := -number2;
-        end if;
-        
+        IF (m = '0') THEN
+            number2 := - number2;
+        END IF;
+
         -- addition
         result := (number1(7) & number1) + (number2(7) & number2);
         c <= result(8);
-        if (result < 0) then
+        IF (result < 0) THEN
             s <= '1';
-        end if;
-        
-        if result(7 DOWNTO 0) = x"00" then
+        END IF;
+
+        IF result(7 DOWNTO 0) = x"00" THEN
             zero <= '1';
-        end if;
-        
-        Q <= std_logic_vector(result(7 DOWNTO 0));
+        END IF;
+
+        Q <= STD_LOGIC_VECTOR(result(7 DOWNTO 0));
     END PROCESS;
 
 END arch;
